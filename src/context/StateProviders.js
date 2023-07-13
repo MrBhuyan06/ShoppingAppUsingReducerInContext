bd5491d233d5e133d511c101c575fcf9981b41ff
Abhishek Bhuyan <abhisekhbhuyan0606@gmail.com>
2023-07-13 23:44:38 +0530

    Added all the reducers

--- src/context/StateProviders.js
+++ src/context/StateProviders.js
@@ -1,12 +1,22 @@
 import React, { createContext, useContext, useReducer } from "react";
-import { reducer } from "./reducers.js";
+import { productReducer, reducer } from "./reducers.js";
 import { initial } from "./initial.js";
 
 const Carts = createContext();
 console.log(Carts);
 export const StateProviders = ({ children }) => {
   const [state, dispath] = useReducer(reducer, initial);
-  return <Carts.Provider value={{ state, dispath }}>{children}</Carts.Provider>;
+  const [productState, productdispatch] = useReducer(productReducer, {
+    bystock: false,
+    byfastDelivery: false,
+    byRating: 0,
+    searchQuery: "",
+  });
+  return (
+    <Carts.Provider value={{ state, dispath, productState, productdispatch }}>
+      {children}
+    </Carts.Provider>
+  );
 };
 StateProviders;
 export const useStateValue = () => useContext(Carts);
